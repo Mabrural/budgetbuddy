8630cc085c9eadd13e027999315a5d65ce3bdc2a
mabrur <mabruralmutaqi@gmail.com>
2023-12-07 21:04:25 +0700

    menambahkan sweet alert pada tagihan.php, ubah.php,tambah.php, hapus.php

--- page/tagihan/hapus.php
+++ page/tagihan/hapus.php
@@ -6,19 +6,53 @@ if(isset($_POST["submit"])) {
     $id_tagihan = isset($_POST["id_tagihan"]) ? $_POST["id_tagihan"] : null;
 
     if ($id_tagihan !== null && hapusTagihan($id_tagihan) > 0 ){
-        echo "
-            <script>
-                alert('Data berhasil dihapus!');
-                document.location.href = '?page=tagihan';
-            </script>
-        ";
+        echo '<link rel="stylesheet" href="./sweetalert2.min.css"></script>';
+		echo '<script src="./sweetalert2.min.js"></script>';
+		echo "<script>
+		setTimeout(function () { 
+			swal.fire({
+				
+				title               : 'Berhasil',
+				text                :  'Data berhasil dihapus',
+				//footer              :  '',
+				icon                : 'success',
+				timer               : 2000,
+				showConfirmButton   : true
+			});  
+		},10);   setTimeout(function () {
+			window.location.href = '?page=tagihan'; //will redirect to your blog page (an ex: blog.html)
+		}, 2000); //will call the function after 2 secs
+		</script>";
+        // echo "
+        //     <script>
+        //         alert('Data berhasil dihapus!');
+        //         document.location.href = '?page=tagihan';
+        //     </script>
+        // ";
     } else {
-        echo "
-            <script>
-                alert('Data gagal dihapus!');
-                document.location.href = '?page=tagihan';
-            </script>
-        ";
+        echo '<link rel="stylesheet" href="./sweetalert2.min.css"></script>';
+		echo '<script src="./sweetalert2.min.js"></script>';
+		echo "<script>
+		setTimeout(function () { 
+			swal.fire({
+				
+				title               : 'Gagal',
+				text                :  'Data gagal dihapus',
+				//footer              :  '',
+				icon                : 'error',
+				timer               : 2000,
+				showConfirmButton   : true
+			});  
+		},10);   setTimeout(function () {
+			window.location.href = '?page=tagihan'; //will redirect to your blog page (an ex: blog.html)
+		}, 2000); //will call the function after 2 secs
+		</script>";
+        // echo "
+        //     <script>
+        //         alert('Data gagal dihapus!');
+        //         document.location.href = '?page=tagihan';
+        //     </script>
+        // ";
     }
 }
 
