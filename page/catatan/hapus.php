2c2bd43b2afd022a8a2a76d6a8e982ba4b0d23f1
mabrur <mabruralmutaqi@gmail.com>
2023-12-07 21:10:16 +0700

    menambahkan sweet alert pada catatan.php, tambah.php, ubah.php, hapus.php

--- page/catatan/hapus.php
+++ page/catatan/hapus.php
@@ -6,19 +6,53 @@ if(isset($_POST["submit"])) {
     $id_catatan = isset($_POST["id_catatan"]) ? $_POST["id_catatan"] : null;
 
     if ($id_catatan !== null && hapusCatatan($id_catatan) > 0 ){
-        echo "
-            <script>
-                alert('Data berhasil dihapus!');
-                document.location.href = '?page=catatan';
-            </script>
-        ";
+        echo '<link rel="stylesheet" href="./sweetalert2.min.css"></script>';
+		echo '<script src="./sweetalert2.min.js"></script>';
+		echo "<script>
+		setTimeout(function () { 
+			swal.fire({
+				
+				title               : 'Berhasil',
+				text                :  'Data berhasil dihapus',
+				//footer              :  '',
+				icon                : 'success',
+				timer               : 2000,
+				showConfirmButton   : true
+			});  
+		},10);   setTimeout(function () {
+			window.location.href = '?page=catatan'; //will redirect to your blog page (an ex: blog.html)
+		}, 2000); //will call the function after 2 secs
+		</script>";
+        // echo "
+        //     <script>
+        //         alert('Data berhasil dihapus!');
+        //         document.location.href = '?page=catatan';
+        //     </script>
+        // ";
     } else {
-        echo "
-            <script>
-                alert('Data gagal dihapus!');
-                document.location.href = '?page=catatan';
-            </script>
-        ";
+        echo '<link rel="stylesheet" href="./sweetalert2.min.css"></script>';
+		echo '<script src="./sweetalert2.min.js"></script>';
+		echo "<script>
+		setTimeout(function () { 
+			swal.fire({
+				
+				title               : 'Gagal',
+				text                :  'Data gagal dihapus',
+				//footer              :  '',
+				icon                : 'error',
+				timer               : 2000,
+				showConfirmButton   : true
+			});  
+		},10);   setTimeout(function () {
+			window.location.href = '?page=catatan'; //will redirect to your blog page (an ex: blog.html)
+		}, 2000); //will call the function after 2 secs
+		</script>";
+        // echo "
+        //     <script>
+        //         alert('Data gagal dihapus!');
+        //         document.location.href = '?page=catatan';
+        //     </script>
+        // ";
     }
 }
 
